295150c782cc5c80e3b12a27031de66e450a7b89
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2026-04-18 11:05:39 +0700

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: 'false',
+	darkMode: false,
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
